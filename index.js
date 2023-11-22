763df1e7a021a1908e8c132fe8757748647fea70
heathermtuedwa <heathermtuedwa@gmail.com>
2023-11-22 16:09:21 +0200

    Added files

---
+++ index.js
@@ -0,0 +1,7 @@
+function displayResult() {
+    let ageInput = document.getElementById("age").value;
+    let days = ageInput * 365.25;
+    document.getElementById("result").innerHTML = "Days: " + days.toFixed(2);
+}
+
+document.getElementById("button").addEventListener("click", displayResult);
